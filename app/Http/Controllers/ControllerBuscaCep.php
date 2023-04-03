d775871e47be145d7b83bff5aa7946771d99ad9f
Laravel <salvadorbba@gmail.com>
2023-04-03 07:45:57 -0300

    App busca cep

--- app/Http/Controllers/ControllerBuscaCep.php
+++ app/Http/Controllers/ControllerBuscaCep.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class ControllerBuscaCep extends Controller
 {
@@ -10,75 +11,20 @@ class ControllerBuscaCep extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
+     * 
+     * instalação
+     * 1º Crie um controller: php artisan make:controller ControllerBuscaCep  --resource
+     * 2º Instalar a biblioteca GuzzleHttp: comando-> composer require guzzlehttp/guzzle
+     * 3º Criar a rota para a busca de CEP : Route::get('cep', [ControllerBuscaCep::class, 'buscaCep']);
+     * 4º Inicie o servidor: php artisan serve
      */
-    public function index()
+    public function buscaCep(Request $request)
     {
-        //
-    }
+        $client = new Client();
+        $response = $client->request('GET', "https://viacep.com.br/ws/$request->cep/json/");
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $data = json_decode($response->getBody(), true);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('XML', [ControllerBuscaCep::class, 'index']);
+Route::get('cep', [ControllerBuscaCep::class, 'buscaCep']);
